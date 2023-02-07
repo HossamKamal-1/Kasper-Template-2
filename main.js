b1d1085fb119cd2cf03dce87c162f7def69ff7e0
kingbero44 <kingbero55@gmail.com>
2023-02-07 12:15:36 +0200

    refactor increasecountonscroll function and added filtered gallery script

--- main.js
+++ main.js
@@ -4,18 +4,33 @@ let skillsContainer = document.querySelector(".skill-sec");
 let skillsProgressSpans = document.querySelectorAll(
   ".skill-sec .skills .prog span"
 );
-let startFlags = [false];
 let dropDownLink = document.querySelector("header nav .toggle-menu");
 let megaMenu = document.querySelector("header .container nav ul");
 dropDownLink.addEventListener("click", function (e) {
   e.currentTarget.classList.toggle("open");
 });
-
+let switcherElements = document.querySelectorAll(".portfolio .shuffle li");
+let galleryElements = document.querySelectorAll(
+  ".portfolio .image-container .box"
+);
+let increaseCountOnScrollStats = increaseCountOnScroll(
+  statContainer,
+  statElements,
+  800
+);
 window.addEventListener("scroll", () => {
   // stats Element
-  increaseCountOnScroll(statContainer, statElements, 0, 800);
+  increaseCountOnScrollStats();
   // skills Element
   setElementWidthOnScroll(skillsContainer, skillsProgressSpans);
+});
+
+document.addEventListener("click", (e) => {
+  switcherElements.forEach((switcherEl) => {
+    if (e.target === switcherEl && !switcherEl.classList.contains("active")) {
+      switchElementHandler(e.target);
+    }
+  });
 });
 
 function setElementWidthOnScroll(section, spans) {
@@ -34,26 +49,34 @@ function setElementWidthOnScroll(section, spans) {
   });
 }
 
-function increaseCountOnScroll(section, numberSpans, index, offsetToStart) {
-  // out of scrolling zone ==> startFlag false, back numbers to 0
-  if (window.scrollY < section.offsetTop - offsetToStart) {
-    startFlags[index] = false;
-    numberSpans.forEach((numSpan) => {
-      numSpan.textContent = "0";
-    });
-    return;
-  }
-  // in scrolling zone ==> increase element Count + startFlag True
-  if (!startFlags[index]) {
-    numberSpans.forEach((numSpan) => increaseElementCount(numSpan, index));
-    startFlags[index] = true;
-  }
+function increaseCountOnScroll(section, numberSpans, offsetToStart) {
+  let startFlag = false;
+  // Closure
+  return function () {
+    // out of scrolling zone ==> startFlag false, back numbers to 0
+    if (window.scrollY < section.offsetTop - offsetToStart) {
+      startFlag = false;
+      numberSpans.forEach((numSpan) => {
+        numSpan.textContent = "0";
+      });
+      return;
+    }
+    // in scrolling zone ==> increase element Count + startFlag True
+    if (!startFlag) {
+      numberSpans.forEach((numSpan) => {
+        let increaseFunc = increaseElementCount(numSpan);
+        let count = setInterval(() => {
+          increaseFunc(count);
+        }, 800 / numSpan.dataset.goal);
+      });
+      startFlag = true;
+    }
+  };
 }
-
 function increaseElementCount(element) {
   let goal = element.dataset.goal;
-  let count = setInterval(startIncrease, 800 / goal);
-  function startIncrease() {
+  // Closure
+  return function startIncrease(count) {
     if (element.textContent.replace(",", "") == goal) {
       clearInterval(count);
       return;
@@ -66,5 +89,45 @@ function increaseElementCount(element) {
         element.textContent.length - 3
       )},${element.textContent.slice(element.textContent.length - 3)}`;
     }
-  }
+  };
+}
+
+function switchElementHandler(element) {
+  removeActiveFromAllSwitchers();
+  addClassActiveToCurrentSwitcherElement(element);
+  displaySelectedCategoryGalleryElements(element);
+}
+
+function wait(milliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+async function removeGalleryElements() {
+  galleryElements.forEach((galleryEl) => {
+    galleryEl.classList.add("shrink");
+  });
+  await wait(200);
+  galleryElements.forEach((galleryEl_1) => {
+    galleryEl_1.style.display = "none";
+  });
+}
+
+async function displaySelectedCategoryGalleryElements(element) {
+  await removeGalleryElements();
+  document.querySelectorAll(element.dataset.filter).forEach((element) => {
+    element.style.display = "block";
+  });
+  await wait(100);
+  document.querySelectorAll(element.dataset.filter).forEach((element) => {
+    element.classList.remove("shrink");
+  });
+}
+function removeActiveFromAllSwitchers() {
+  switcherElements.forEach((switcherEl) =>
+    switcherEl.classList.remove("active")
+  );
+}
+function addClassActiveToCurrentSwitcherElement(element) {
+  element.classList.add("active");
 }
